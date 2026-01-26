c7732671db1ca20b520159b30af5e93943adbd60
tranvanhung2003 <tranvanhan29042007@gmail.com>
2026-01-26 22:16:52 +0700

    kiểu dữ liệu nguyên thủy (primitive types)

--- src/chapter2/HelloWorld.java
+++ src/chapter2/HelloWorld.java
@@ -2,18 +2,6 @@ package chapter2;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int abc = 10;
-        // abc
-        // 1abc
-        // ab_c
-        // _abc
-        // $abc
-        // ab-c
-        // _123
-        // $123
-        // if
-        // $if
-
-        System.out.println("Hello World");
+        byte age = 20;
     }
 }
