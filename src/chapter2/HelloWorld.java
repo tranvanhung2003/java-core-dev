95609eb6e019bdbf20b9885802033dc492ae8d16
tranvanhung2003 <tranvanhan29042007@gmail.com>
2026-01-26 17:55:37 +0700

    variables

---
+++ src/chapter2/HelloWorld.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int abc = 10;
+        // abc
+        // 1abc
+        // ab_c
+        // _abc
+        // $abc
+        // ab-c
+        // _123
+        // $123
+        // if
+        // $if
+
+        System.out.println("Hello World");
+    }
+}
