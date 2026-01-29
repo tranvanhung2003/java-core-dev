ccebdc8c792c9a83ef131672af87b55c2eb657c0
tranvanhung2003 <tranvanhan29042007@gmail.com>
2026-01-29 23:08:41 +0700

    update

--- src/chapter2/HelloWorld.java
+++ src/chapter2/HelloWorld.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
 
         String name = "Tran Van Hung";
 
-        System.out.print("Nhap tuoi cua ban: ");
-        int age = scanner.nextInt();
+        System.out.println("Nhap tuoi cua ban:");
+        int age = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Ten cua ban la: %s va tuoi cua ban la: %d".formatted(name, age));
+        System.out.println("Ten cua ban la: %s, tuoi cua ban la: %d".formatted(name, age));
 
         float pi = 3.1415164755f;
         System.out.println("Gia tri cua pi la: %.2f".formatted(pi));
+
+        scanner.close();
     }
 }
---
+++ src/chapter2/Lab1Bai1.java
@@ -0,0 +1,6 @@
+package chapter2;
+
+public class Lab1Bai1 {
+    public static void main(String[] args) {
+    }
+}
